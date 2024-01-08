e1c95275bd7c140d56dd7f0883b3ca3195c0c821
KevinMuniz1 <munizkevin@outlook.com>
2024-01-08 14:15:08 -0500

    made a view for the flag image in order to make the body more organized

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,8 +7,17 @@
 
 import SwiftUI
 
+struct FlagView: View {
+    let name: String
+    var body: some View {
+        Image(name)
+            .clipShape(.buttonBorder)
+            .shadow(radius: 5)
+    }
+}
+
 struct ContentView: View {
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State var titleName = ""
     @State private var showScore = false
@@ -39,9 +48,7 @@ struct ContentView: View {
                         Button {
                             displayTitleAndCheckAnswer(number)
                         } label: {
-                            Image(countries[number])
-                                .clipShape(.buttonBorder)
-                                .shadow(radius: 5)
+                            FlagView(name: countries[number])
                         }
                     }
                 }.frame(maxWidth: .infinity)
